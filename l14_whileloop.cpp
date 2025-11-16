cb5f99919434d80a1dc0bab1cea28d37bdb70a5f
dwardo4321 <chidatapiwa@gmail.com>
2025-11-16 00:39:12 +0000

    Added

---
+++ l14_whileloop.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using std::cout;
+
+/*
+So getline() is used when you want a full sentence, whole
+ line, or anything with spaces.
+*/
+
+void demo1(){
+std::string name;
+    
+    while(name.empty()){
+        cout << "Enter her name: ";
+        std::getline(std::cin, name);
+    }
+    std::cout << "She's special isnt she?" << std::endl;
+}
+
+void demo2(){
+    double num;
+
+    do{
+        cout << "Enter number: ";
+        std::cin >> num;
+    }
+    while(num <= 0);
+    cout << "Great";
+}
+
+/*
+void demo3(){
+std::string name;
+    
+    while(1 == 1){
+        cout << "HELP!!!";
+    }
+    std::cout << "YES!!!";
+}
+*/
+
+int main(){
+    demo1();
+    demo2();
+    //demo3();
+    return 0;
+}
